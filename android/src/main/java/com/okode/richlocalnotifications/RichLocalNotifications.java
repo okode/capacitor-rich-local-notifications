f2c6f82453573cef54e210053a34dfbb8bcade10
Rubén Panadero <rpanadero@okode.com>
2019-07-16 15:20:10 +0200

    Refactoring rich local notifications and using its own handler

--- android/src/main/java/com/okode/richlocalnotifications/RichLocalNotifications.java
+++ android/src/main/java/com/okode/richlocalnotifications/RichLocalNotifications.java
@@ -1,5 +1,7 @@
 package com.okode.richlocalnotifications;
 
+import android.content.Intent;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
@@ -7,6 +9,7 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
 import org.json.JSONObject;
+
 
 @NativePlugin()
 public class RichLocalNotifications extends Plugin {
@@ -19,14 +22,24 @@ public class RichLocalNotifications extends Plugin {
         manager.createDefaultNotificationChannel();
     }
 
+    @Override
+    protected void handleOnNewIntent(Intent data) {
+        super.handleOnNewIntent(data);
+        if (!Intent.ACTION_MAIN.equals(data.getAction())) {
+            return;
+        }
+        JSObject dataJson = manager.handleNotificationActionPerformed(data);
+        if (dataJson != null) {
+            notifyListeners("richLocalNotificationActionPerformed", dataJson, true);
+        }
+    }
+
     @PluginMethod()
     public void show(PluginCall call) {
         JSONObject notification = call.getObject("notification");
-        RichLocalNotification richLocalNotification = RichLocalNotification.buildNotification(getContext(), notification);
-        if (richLocalNotification == null) {
-            call.error("Provided notification format is invalid");
-            return;
-        }
+        RichLocalNotification richLocalNotification = RichLocalNotification.Builder
+                .from(getContext(), notification)
+                .build();
         Integer id = manager.show(call, richLocalNotification);
         call.success(new JSObject().put("id", id));
     }
